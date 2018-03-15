acbc746f3d5fe44833658397db23d6669604e3bb
guekho64 <guekho64@users.noreply.github.com>
2018-03-15 01:27:48 -0600

    Update Test.java

--- Misc/Test.java
+++ Misc/Test.java
@@ -1,8 +1,11 @@
 package guekho64.Test;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +16,7 @@ public final class Main {
   public static final void main(final String... args) {
 
     final int objectivePercentage = Solver(args[0]);
-    
+
     if (args[0].isEmpty()) {
       System.out.println("Error -1");
       System.exit(-1);
@@ -26,99 +29,169 @@ public final class Main {
       System.out.println("Error -3");
       System.exit(-3);
     }
-    
-    
+
+
     final SimpleImmutableEntry<String, Integer> x = new SimpleImmutableEntry<String, Integer>("x", 0);
     final SimpleImmutableEntry<String, Integer> y = new SimpleImmutableEntry<String, Integer>("y", 100);
     final SimpleImmutableEntry<String, Integer> m = new SimpleImmutableEntry<String, Integer>("m", ((x.getValue() + y.getValue())/2));
     final SimpleImmutableEntry<String, Integer> p = new SimpleImmutableEntry<String, Integer>("p", ((m.getValue() + y.getValue())/2));
     final SimpleImmutableEntry<String, Integer> o = new SimpleImmutableEntry<String, Integer>("o", ((x.getValue() + m.getValue())/2));
-    
-    
+
+
     final String baseCommand = ("python -c" + " ") + (
         (( "x=(#*1.0);".replaceAll("\\#", String.valueOf(x.getValue())) +
-        "y=($*1.0);".replaceAll("\\$", String.valueOf(y.getValue()))) +
-        ( "m=(%*1.0);".replaceAll("\\%", String.valueOf(m.getValue())) +
-        "p=(&*1.0);".replaceAll("\\&", String.valueOf(p.getValue())))) +
+            "y=($*1.0);".replaceAll("\\$", String.valueOf(y.getValue()))) +
+            ( "m=(%*1.0);".replaceAll("\\%", String.valueOf(m.getValue())) +
+                "p=(&*1.0);".replaceAll("\\&", String.valueOf(p.getValue())))) +
         "o=(?*1.0);".replaceAll("\\?", String.valueOf(o.getValue())));
-    
-    class PythonCommandObject {
-      final String command;
-      PythonCommandObject (final String command) {
-        this.command = Main.PythonCommand(baseCommand, command);
-      }
-      String getCommand () {
-        return command;
-      }
-      String executeCommand () {
-        return Exec(command);
-      }
-    }
-    
+
+
+
     /** Math **/
-    
+
     boolean firstAttempt = true;
-    
-    float percentage;
-    
-    int interestingIndex1;
-    int interestingIndex2;
-    
+
+    float percentage = -640;
+
+    int interestingIndex1 = -640;
+    int interestingIndex2 = -640;
+
     int preciseIndex = -1;
-    
+
     Float finalPercentage = null;
-    
+
     @SuppressWarnings("serial")
     List<Boolean> bList = new ArrayList<Boolean>() {{
-      add(y.getValue() > objectivePercentage);
-      add(p.getValue() > objectivePercentage);
-      add(m.getValue() > objectivePercentage);
-      add(o.getValue() > objectivePercentage);
-      add(x.getValue() > objectivePercentage);
-    }};
-    
-    @SuppressWarnings("serial")
-    List<Boolean> bList2 = new ArrayList<Boolean>() {{
       add(y.getValue() == objectivePercentage);
       add(p.getValue() == objectivePercentage);
       add(m.getValue() == objectivePercentage);
       add(o.getValue() == objectivePercentage);
       add(x.getValue() == objectivePercentage);
     }};
-    
-    String u;
-    String v;
-    
+
+    @SuppressWarnings("serial")
+    List<SimpleImmutableEntry<String, Boolean>> bList2 = new ArrayList<SimpleImmutableEntry<String, Boolean>>() {{
+      add(new SimpleImmutableEntry<String, Boolean>("y", (y.getValue() > objectivePercentage)));
+      add(new SimpleImmutableEntry<String, Boolean>("p", (p.getValue() > objectivePercentage)));
+      add(new SimpleImmutableEntry<String, Boolean>("m", (m.getValue() > objectivePercentage)));
+      add(new SimpleImmutableEntry<String, Boolean>("o", (o.getValue() > objectivePercentage)));
+      add(new SimpleImmutableEntry<String, Boolean>("x", (x.getValue() > objectivePercentage)));
+    }};
+
+    String print = "print(#)";
+
+    String u = null;
+    String v = null;
+
+    String builder = null;
+
+    Integer justACounter = 0;
+
     try {
 
       while (true) {
 
         if (firstAttempt) {
 
-          for (int counter = 0; counter < bList2.size(); ++counter) {
-            if (bList2.get(counter)) {
+          for (int counter = 0; counter < bList.size(); ++counter) {
+            if (bList.get(counter)) {
               preciseIndex = counter;
               throw new ZipException();
             }
           }
 
-          for (int counter = 0; counter < bList.size(); ++counter) {
-            if ((bList.get(counter) == false) && (bList.get(counter++) == true)) {
-              interestingIndex1 = counter;
-              interestingIndex2 = counter++;
+          for (int counter = 0; counter < bList2.size(); ++counter) {
+            if (counter != 4) {
+              if ((bList2.get(counter).getValue() == true) && (bList2.get(counter + 1).getValue() == false)) {
+                interestingIndex1 = counter;
+                interestingIndex2 = counter + 1;
+
+                builder = ByTwo((bList2.get(interestingIndex1).getKey() + "+") + bList2.get(interestingIndex2).getKey());
+
+                percentage = Float.valueOf(ExecPythonCommand(baseCommand, print.replaceAll("\\#", builder)));
+
+                if (Verify(percentage, objectivePercentage)) {
+                  finalPercentage = percentage;
+                }
+
+              }
             }
             else {
-              throw new RuntimeException("What just happened?");
+              //throw new RuntimeException(String.valueOf(builder));
             }
           }
-          
-          if 
 
           firstAttempt = false;
+          
 
         }
+
         else {
 
+          if ((u == null) && (v == null)) {
+            
+            
+            if (percentage > objectivePercentage) {
+
+              u = builder;
+              builder = ByTwo((builder + "+") + String.valueOf(bList2.get(interestingIndex2).getKey()));
+              
+              
+
+            }
+
+            else if (percentage < objectivePercentage) {
+
+              v = builder;
+              builder = ByTwo((builder + "+") + String.valueOf(bList2.get(interestingIndex1).getKey()));
+
+            }
+
+            percentage = Float.valueOf(ExecPythonCommand(baseCommand, print.replaceAll("\\#", builder)));
+
+          }
+          else {
+            
+            
+            if (u != null) {
+              if (percentage > objectivePercentage) {
+                
+                //System.out.println(builder);
+                
+                if (justACounter == 10) {
+                  
+                  try {
+                    PrintWriter ggg = new PrintWriter(new File("a.txt"));
+                    ggg.print(builder);
+                    ggg.close();
+                  } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                  }
+                  
+                 
+                  
+                  throw new RuntimeException();
+                }
+                else {
+                  justACounter += 1;
+                }
+                
+                u = builder;
+                builder = ByTwo((builder + "+") + String.valueOf(bList2.get(interestingIndex2).getKey()));
+              }
+              else {
+                
+                v = builder;
+                builder = ByTwo((builder + "+") + u);
+              }
+            }
+            else if (v != null) {
+            }
+            
+            percentage = Float.valueOf(ExecPythonCommand(baseCommand, print.replaceAll("\\#", builder)));
+            
+          }
         }
 
       }
@@ -126,7 +199,7 @@ public final class Main {
     catch (ZipException fictionalProblem) {
       assert true;
     }
-    
+
     if (preciseIndex != -1) {
       switch (preciseIndex) {
         case (0): {
@@ -154,17 +227,17 @@ public final class Main {
         }
       }
     }
-    
-//    if (finalPercentage != null) {
-//      System.out.println(finalPercentage.floatValue());
-//    }
-//    else {
-//      throw new RuntimeException("Duh!");
-//    }
-    
+
+    //    if (finalPercentage != null) {
+    //      System.out.println(finalPercentage.floatValue());
+    //    }
+    //    else {
+    //      throw new RuntimeException("Duh!");
+    //    }
+
 
   }
-  
+
   public static final int Solver (final String numberInString) {
     try {
       return (int) Integer.valueOf(numberInString);
@@ -173,33 +246,33 @@ public final class Main {
       return -64;
     }
   }
-  
+
   public static final String Exec (final String command) {
 
-   final  StringBuffer output = new StringBuffer();
+    final  StringBuffer output = new StringBuffer();
 
-   final Process process = Solver2(command);
-   
-   if (process == null) {
-     throw new RuntimeException("Ded");
-   }
-      
-   Solver3(process);
-   
-   final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-   
-   String line = "";
+    final Process process = Solver2(command);
 
-   while ((line = Solver4(reader)) != null) {
-     output.append(line + "\n");
-   }
-   
-  
-   
-   return output.toString();
+    if (process == null) {
+      throw new RuntimeException("Ded");
+    }
+
+    Solver3(process);
+
+    final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+    String line = "";
+
+    while ((line = Solver4(reader)) != null) {
+      output.append(line + "\n");
+    }
+
+
+
+    return output.toString();
 
   }
-  
+
   public static final Process Solver2 (final String command) {
     try {
       return Runtime.getRuntime().exec(command);
@@ -208,7 +281,7 @@ public final class Main {
       return null;
     }
   }
-  
+
   public static final void Solver3 (final Process process) {
     try {
       process.waitFor();
@@ -217,7 +290,7 @@ public final class Main {
       throw new RuntimeException("Ded2");
     }
   }
-  
+
   public static final String Solver4 (final BufferedReader bufferedReader) {
     try {
       return bufferedReader.readLine();
@@ -230,5 +303,30 @@ public final class Main {
   public static final String PythonCommand (final String pythonCommand, final String python) {
     return (pythonCommand+ (python + ";"));
   }
-  
+
+  public static final String ExecPythonCommand (final String pythonCommand, final String python) {
+    return Exec(PythonCommand(pythonCommand, python));
+  }
+
+  public static final String Wrap (final String string) {
+    return "(#)".replaceAll("\\#", string);
+  }
+
+  public static final String ByTwo (final String string) {
+    return Wrap(Wrap(string) + "*0.5");
+  }
+
+  public static final boolean Verify (final float objectiveNumber, final float tolerance, final float actualNumber) {
+    if ( !(actualNumber > (objectiveNumber + tolerance)) && !(actualNumber < (objectiveNumber - tolerance)) ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public static final boolean Verify (final float objectiveNumber, final float actualNumber) {
+    return Verify(objectiveNumber, 0.1F, actualNumber);
+  }
+
 }
